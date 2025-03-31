76eac690cb418b03e21f049f2b9096b45481afe2
Ben Cressman <89537293+bencres@users.noreply.github.com>
2025-03-30 19:21:35 -0500

    `test_correct_output` now imports from src.module, rather than module.

--- c00/test_helloworld.py
+++ c00/test_helloworld.py
@@ -20,6 +20,6 @@ def test_correct_output(monkeypatch):
     with monkeypatch.context() as mock_context:
         mock_context.setattr(sys, "stdout", mocked_stdout)
         sys.modules.pop(MODULE_NAME, None)
-        importlib.import_module(name=MODULE_NAME)
+        importlib.import_module(name=f"src.{MODULE_NAME}")
     expected_result = ("Hello World!\n")
     assert_equal (expected_result, mocked_stdout.getvalue())
