1cba5bbd3974bb5c47627e0db5ae37ea711fb060
Ben Cressman <89537293+bencres@users.noreply.github.com>
2025-04-24 13:14:49 -0500

    Bugfix: test would import the same module from disk every time, whichever was earliest in sys.path, regardless of which dir the test was run from

--- c00/test_hello_world.py
+++ c00/test_hello_world.py
@@ -1,12 +1,19 @@
 import importlib
 import sys
 import io
+import os
+from pathlib import Path
 
 from check_pfda.utils import (assert_script_exists, build_user_friendly_err,
                               get_module_in_src)
 
 
+cwd_src = os.path.join(os.getcwd(), "src")
+if cwd_src not in sys.path:
+    sys.path.insert(0, cwd_src)
+
 MODULE_NAME = get_module_in_src()
+
 ACCEPTED_DIRS = ["src"]
 
 
@@ -18,7 +25,11 @@ def test_correct_output(monkeypatch):
     mocked_stdout = io.StringIO()
     with monkeypatch.context() as mock_context:
         mock_context.setattr(sys, "stdout", mocked_stdout)
-        sys.modules.pop(MODULE_NAME, None)
-        importlib.import_module(name=MODULE_NAME)
+        try:
+            sys.modules.pop(MODULE_NAME, None)
+            importlib.import_module(MODULE_NAME)
+        finally:
+            if cwd_src in sys.path:
+                sys.path.remove(cwd_src)
     expected_result = ("Hello World!\n")
     assert mocked_stdout.getvalue() == expected_result, (build_user_friendly_err(mocked_stdout.getvalue(), expected_result))
